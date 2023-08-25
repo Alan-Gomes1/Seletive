0cf2c3ab3d3a7a9caf916fcd65015377607ab0d1
Alan-Gomes1 <alan.gomes.ag28@gmail.com>
2023-08-24 23:17:58 -0300

    Adicionando teste ConfirmarLogin

--- empresa/tests.py
+++ empresa/tests.py
@@ -33,3 +33,14 @@ class ConfirmarCadastroTeste(TestCase):
     def teste_confirmar_cadastro_view_nao_aceita_requisicao_get(self):
         resposta = self.client.get(reverse('confirmar_cadastro'), {})
         self.assertEqual(resposta.status_code, 405)
+
+
+class ConfirmarLoginTeste(TestCase):
+    def setUp(self):
+        self.resposta = self.client.post(reverse('confirmar_login'))
+
+    def teste_confirmar_login_view_retorna_status_code_200(self):
+        self.assertEqual(self.resposta.status_code, 200)
+
+    def teste_confirmar_login_conteudo(self):
+        self.assertIn('login', self.resposta.content.decode('utf-8'))
