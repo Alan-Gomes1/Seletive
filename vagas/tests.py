bc67890697b6ac0b44c0f68d4a54eee4d9509312
Alan-Gomes1 <alan.gomes.ag28@gmail.com>
2023-09-24 23:33:41 -0300

    Adicionando teste nova_vaga_cadastro_invalido

--- vagas/tests.py
+++ vagas/tests.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
-
+from django.core.exceptions import ValidationError
 from empresa.models import Empresa, Tecnologias, Vagas
 
 
@@ -44,3 +44,16 @@ class NovaVagaTeste(TestCase):
 
         self.assertEqual(self.vaga.tecnologias_dominadas.count(), 1)
         self.assertEqual(self.vaga.tecnologias_estudar.count(), 0)
+
+    def teste_nova_vaga_cadastro_invalido(self):
+        valores = {
+            'titulo': ' ',
+            'nivel_experiencia': ' ',
+            'data_final': ' ',
+            'email': ' ',
+            'status': ' ',
+        }
+
+        for campo, valor in valores.items():
+            with self.subTest(campo=campo):
+                self.assertNotEqual(getattr(self.vaga, campo), valor)
